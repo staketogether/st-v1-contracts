276819e9f707ab5603bf29119b13020c445ad8b5
Fabricio Miranda <fabriciomirandabr@gmail.com>
2023-06-17 11:05:52 -0300

    feat: add licence BUSL-1.1

--- contracts/SETH.sol
+++ contracts/SETH.sol
@@ -1,5 +1,4 @@
-// SPDX-FileCopyrightText: 2023 Stake Together <info@staketogether.app>
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.18;
 
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
