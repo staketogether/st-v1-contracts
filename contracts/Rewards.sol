d250eeff1ca088cb0a53d60db1069cdde2e40dc6
Fabricio Miranda <fabriciomirandabr@gmail.com>
2023-06-21 16:37:02 -0300

    feat: update oracle system

--- contracts/Rewards.sol
+++ contracts/Rewards.sol
@@ -12,120 +12,71 @@ import './StakeTogether.sol';
 contract Rewards is Ownable, Pausable, ReentrancyGuard {
   StakeTogether public stakeTogether;
 
+  // event EtherReceived(address indexed sender, uint amount);
+
+  // receive() external payable {
+  //   emit EtherReceived(msg.sender, msg.value);
+  // }
+
+  // fallback() external payable {
+  //   emit EtherReceived(msg.sender, msg.value);
+  // }
+
+  function setStakeTogether(address _stakeTogether) external onlyOwner {
+    require(address(stakeTogether) == address(0), 'STAKE_TOGETHER_ALREADY_SET');
+    stakeTogether = StakeTogether(payable(_stakeTogether));
+    emit SetStakeTogether(_stakeTogether);
+  }
+
   /*****************
    ** TIME LOCK **
    *****************/
 
-  event ProposeSetTimeLockDuration(uint256 duration, uint256 executionTime);
-  event ExecuteSetTimeLockDuration(uint256 duration);
-  event ProposeSetDisagreementLimit(uint256 limit, uint256 executionTime);
-  event ExecuteSetDisagreementLimit(uint256 limit);
-  event ProposeAddOracle(address oracle, uint256 executionTime);
-  event ExecuteAddOracle(address oracle);
-  event ProposeRemoveOracle(address oracle, uint256 executionTime);
-  event ExecuteRemoveOracle(address oracle);
+  event ProposeTimeLockAction(string action, uint256 value, address target, uint256 executionTime);
+  event ExecuteTimeLockAction(string action);
 
   struct TimeLockedProposal {
-    address target;
     uint256 value;
+    address target;
     uint256 executionTime;
   }
 
   uint256 public timeLockDuration = 1 days / 15;
   mapping(string => TimeLockedProposal) public timeLockedProposals;
 
-  mapping(address => uint256) public disagreementCounts;
-  uint256 public disagreementLimit = 3;
+  function proposeTimeLockAction(
+    string calldata action,
+    uint256 value,
+    address target
+  ) external onlyOwner {
+    TimeLockedProposal storage proposal = timeLockedProposals[action];
+    require(proposal.executionTime < block.timestamp, 'Previous proposal still pending.');
 
-  function proposeSetTimeLockDuration(uint256 _duration) external onlyOwner {
-    TimeLockedProposal storage proposal = timeLockedProposals['setTimeLockDuration'];
-    require(
-      proposal.executionTime < block.timestamp,
-      'Previous setTimeLockDuration proposal still pending.'
-    );
-
-    proposal.value = _duration;
+    proposal.value = value;
+    proposal.target = target;
     proposal.executionTime = block.timestamp + timeLockDuration;
 
-    emit ProposeSetTimeLockDuration(_duration, proposal.executionTime);
+    emit ProposeTimeLockAction(action, value, target, proposal.executionTime);
   }
 
-  function executeSetTimeLockDuration() external onlyOwner {
-    TimeLockedProposal storage proposal = timeLockedProposals['setTimeLockDuration'];
-    require(block.timestamp >= proposal.executionTime, 'TIME_LOCK_NOT_EXPIRED');
-    timeLockDuration = proposal.value;
-    proposal.executionTime = 0;
-    emit ExecuteSetTimeLockDuration(timeLockDuration);
-  }
+  function executeTimeLockAction(string calldata action) external onlyOwner {
+    TimeLockedProposal storage proposal = timeLockedProposals[action];
+    require(block.timestamp >= proposal.executionTime, 'Time lock not expired yet.');
 
-  function proposeSetDisagreementLimit(uint256 _limit) external onlyOwner {
-    TimeLockedProposal storage proposal = timeLockedProposals['setDisagreementLimit'];
-    require(
-      proposal.executionTime < block.timestamp,
-      'Previous setDisagreementLimit proposal still pending.'
-    );
-
-    proposal.value = _limit;
-    proposal.executionTime = block.timestamp + timeLockDuration;
-
-    emit ProposeSetDisagreementLimit(_limit, proposal.executionTime);
-  }
-
-  function executeSetDisagreementLimit() external onlyOwner {
-    TimeLockedProposal storage proposal = timeLockedProposals['setDisagreementLimit'];
-    require(
-      block.timestamp >= proposal.executionTime,
-      'Time lock for setDisagreementLimit not expired yet.'
-    );
-
-    disagreementLimit = proposal.value;
+    if (keccak256(bytes(action)) == keccak256(bytes('setTimeLockDuration'))) {
+      timeLockDuration = proposal.value;
+    } else if (keccak256(bytes(action)) == keccak256(bytes('setDisagreementLimit'))) {
+      disagreementLimit = proposal.value;
+    } else if (keccak256(bytes(action)) == keccak256(bytes('addOracle'))) {
+      _addOracle(proposal.target);
+    } else if (keccak256(bytes(action)) == keccak256(bytes('removeOracle'))) {
+      _removeOracle(proposal.target);
+    } else if (keccak256(bytes(action)) == keccak256(bytes('setPenalize'))) {
+      penalizeLimit = proposal.value;
+    }
 
     proposal.executionTime = 0;
-
-    emit ExecuteSetDisagreementLimit(disagreementLimit);
-  }
-
-  // Todo: Add Option to Add Multiples Oracles at once
-  function proposeAddOracle(address _oracle) external onlyOwner {
-    TimeLockedProposal storage proposal = timeLockedProposals['addOracle'];
-    require(proposal.executionTime < block.timestamp, 'PREVIOUS_PROPOSAL_PENDING');
-
-    proposal.target = _oracle;
-    proposal.executionTime = block.timestamp + timeLockDuration;
-
-    emit ProposeAddOracle(_oracle, proposal.executionTime);
-  }
-
-  function executeAddOracle() external onlyOwner {
-    TimeLockedProposal storage proposal = timeLockedProposals['addOracle'];
-    require(block.timestamp >= proposal.executionTime, 'TIME_LOCK_PENDING');
-
-    _addOracle(proposal.target);
-
-    proposal.executionTime = 0;
-
-    emit ExecuteAddOracle(proposal.target);
-  }
-
-  function proposeRemoveOracle(address _oracle) external onlyOwner {
-    TimeLockedProposal storage proposal = timeLockedProposals['removeOracle'];
-    require(proposal.executionTime < block.timestamp, 'PREVIOUS_PROPOSAL_PENDING');
-
-    proposal.target = _oracle;
-    proposal.executionTime = block.timestamp + timeLockDuration;
-
-    emit ProposeRemoveOracle(_oracle, proposal.executionTime);
-  }
-
-  function executeRemoveOracle() external onlyOwner {
-    TimeLockedProposal storage proposal = timeLockedProposals['removeOracle'];
-    require(block.timestamp >= proposal.executionTime, 'TIME_LOCK_PENDING');
-
-    _removeOracle(proposal.target);
-
-    proposal.executionTime = 0;
-
-    emit ExecuteRemoveOracle(proposal.target);
+    emit ExecuteTimeLockAction(action);
   }
 
   function isProposalReady(string memory proposalName) public view returns (bool) {
@@ -134,11 +85,102 @@ contract Rewards is Ownable, Pausable, ReentrancyGuard {
   }
 
   /*****************
-   ** REPORTS **
+   ** ORACLES **
    *****************/
 
-  // Todo: Implement Ether Receive
+  modifier onlyOracle() {
+    require(activeOracles[msg.sender] && oraclesBlacklist[msg.sender] < penalizeLimit, 'ONLY_ORACLES');
+    _;
+  }
 
+  event AddOracle(address oracle);
+  event RemoveOracle(address oracle);
+  event SetBunkerMode(bool bunkerMode);
+  event SetOracleQuorum(uint256 newQuorum);
+  event OraclePenalized(
+    address indexed oracle,
+    uint256 penalties,
+    bytes32 faultyReportHash,
+    Report faultyReport,
+    bool removed
+  );
+
+  address[] private oracles;
+  mapping(address => bool) private activeOracles;
+  mapping(address => uint256) public oraclesBlacklist;
+  uint256 public oracleQuorum = 1; // Todo: Mainnet = 3
+  uint256 public penalizeLimit = 3;
+  bool public bunkerMode = false;
+
+  function getOracles() external view returns (address[] memory) {
+    return oracles;
+  }
+
+  function getActiveOracleCount() internal view returns (uint256) {
+    uint256 activeCount = 0;
+    for (uint256 i = 0; i < oracles.length; i++) {
+      if (activeOracles[oracles[i]]) {
+        activeCount++;
+      }
+    }
+    return activeCount;
+  }
+
+  function isOracle(address _oracle) public view returns (bool) {
+    return activeOracles[_oracle] && oraclesBlacklist[_oracle] < penalizeLimit;
+  }
+
+  function setBunkerMode(bool _bunkerMode) external onlyOwner {
+    bunkerMode = _bunkerMode;
+    emit SetBunkerMode(_bunkerMode);
+  }
+
+  function _addOracle(address oracle) internal {
+    require(!activeOracles[oracle], 'ORACLE_EXISTS');
+    oracles.push(oracle);
+    activeOracles[oracle] = true;
+    emit AddOracle(oracle);
+    _updateQuorum();
+  }
+
+  function _removeOracle(address oracle) internal {
+    require(activeOracles[oracle], 'ORACLE_NOT_EXISTS');
+    activeOracles[oracle] = false;
+    emit RemoveOracle(oracle);
+    _updateQuorum();
+  }
+
+  function _updateQuorum() internal onlyOwner {
+    uint256 totalOracles = getActiveOracleCount();
+    uint256 newQuorum = (totalOracles * 8) / 10;
+
+    newQuorum = newQuorum < 3 ? 3 : newQuorum;
+    newQuorum = newQuorum > totalOracles ? totalOracles : newQuorum;
+
+    oracleQuorum = newQuorum;
+    emit SetOracleQuorum(newQuorum);
+  }
+
+  function _penalizeOracle(address oracle, bytes32 faultyReportHash) internal {
+    oraclesBlacklist[oracle]++;
+
+    bool remove = oraclesBlacklist[oracle] >= penalizeLimit;
+    if (remove) {
+      _removeOracle(oracle);
+    }
+
+    emit OraclePenalized(
+      oracle,
+      oraclesBlacklist[oracle],
+      faultyReportHash,
+      reports[faultyReportHash],
+      remove
+    );
+  }
+
+  /*****************
+   ** REPORTS **
+   *****************/
   event ConsensusApproved(uint256 indexed blockNumber, bytes32 reportHash);
   event ConsensusFail(uint256 indexed blockNumber, bytes32 reportHash);
   event ReportQuorumNotAchieved(uint256 indexed blockNumber, bytes32 reportHash);
@@ -149,11 +191,6 @@ contract Rewards is Ownable, Pausable, ReentrancyGuard {
   event SetNextBlock(uint256 newBlock);
 
   event SetRewardsSanityLimit(uint256 amount);
-
-  modifier onlyOracle() {
-    require(isOracle(msg.sender), 'ONLY_ORACLES');
-    _;
-  }
 
   struct Report {
     uint256 blockNumber;
@@ -166,161 +203,97 @@ contract Rewards is Ownable, Pausable, ReentrancyGuard {
     bytes[] exitedValidators;
   }
 
+  mapping(bytes32 => Report) public reports;
+  mapping(uint256 => bytes32[]) public reportsByBlock;
+
   uint256 public reportLastBlock = 0;
   uint256 public reportNextBlock = 1;
   uint256 public reportFrequency = 1;
-  uint256 public reportQuorum = 1; // Todo: Mainnet = 3
 
-  address[] private oracles;
-  mapping(address => bool) public oraclesBlacklist;
-  mapping(bytes32 => Report[]) public oracleReport;
+  uint256 public disagreementLimit = 3;
 
-  mapping(uint256 => uint256) private consensusBlock;
-  mapping(uint256 => uint256) private reportBlockCount;
-  mapping(bytes32 => uint256) private reportHashCount;
+  function submitReport(
+    uint256 blockNumber,
+    uint256 beaconBalance,
+    uint256 totalRewardsAmount,
+    uint256 totalRewardsShares,
+    uint256 stakeTogetherShares,
+    uint256 operatorShares,
+    uint256 poolShares,
+    bytes[] calldata exitedValidators
+  ) external onlyOracle whenNotPaused {
+    require(blockNumber > reportNextBlock, 'Invalid blockNumber');
+    require(blockNumber <= block.number, 'Block number is in the future');
+    require(block.number - blockNumber <= 256, 'Block is too old');
 
-  uint256 public rewardsSanityLimit = 0.01 ether;
-  bool bunkerMode = false;
-
-  function submitBlockReport(uint256 _reportBlock, Report memory _report) public onlyOracle {
-    require(address(stakeTogether) != address(0), 'STAKE_TOGETHER_NOT_SET');
-
-    if (block.number >= reportNextBlock + reportFrequency) {
-      reportNextBlock = reportNextBlock + reportFrequency;
-      emit SetNextBlock(reportNextBlock);
-    }
-
-    require(block.number >= reportNextBlock, 'TOO_EARLY_TO_REPORT');
-
-    _validBlockReport(_report);
-
-    bytes32 reportHash = keccak256(abi.encode(_report));
-
-    reportHashCount[reportHash]++;
-    reportBlockCount[_reportBlock]++;
-
-    if (reportBlockCount[_reportBlock] >= reportQuorum) {
-      if (reportHashCount[reportHash] >= reportQuorum) {
-        approveBlockConsensus(reportHash);
-      } else {
-        // Todo: Double Check on this logic
-        disagreementCounts[msg.sender]++;
-        if (disagreementCounts[msg.sender] >= disagreementLimit) {
-          _addOracleBlacklist(msg.sender);
-        }
-        emit ConsensusFail(_reportBlock, reportHash);
-      }
-    }
-  }
-
-  function approveBlockConsensus(bytes32 _reportsHash) internal nonReentrant {
-    reportLastBlock = reportNextBlock;
-    reportNextBlock = block.number + reportFrequency;
-    emit SetNextBlock(reportNextBlock);
-
-    // Todo: Execute actions
-
-    emit ConsensusApproved(reportNextBlock, _reportsHash);
-  }
-
-  function setReportMaxFrequency(uint256 newFrequency) external onlyOwner {
-    reportFrequency = newFrequency;
-    emit SetReportMaxFrequency(newFrequency);
-  }
-
-  function _validBlockReport(Report memory _report) internal pure {
-    require(
-      _report.totalRewardsShares ==
-        _report.stakeTogetherShares + _report.operatorShares + _report.poolShares,
-      'INVALID_REWARDS_SHARES'
+    bytes32 reportHash = keccak256(
+      abi.encode(
+        blockNumber,
+        beaconBalance,
+        totalRewardsAmount,
+        totalRewardsShares,
+        stakeTogetherShares,
+        operatorShares,
+        poolShares,
+        exitedValidators
+      )
     );
+
+    reports[reportHash] = Report(
+      blockNumber,
+      beaconBalance,
+      totalRewardsAmount,
+      totalRewardsShares,
+      stakeTogetherShares,
+      operatorShares,
+      poolShares,
+      exitedValidators
+    );
+
+    reportsByBlock[blockNumber].push(reportHash);
   }
 
-  /*****************
-   ** ORACLES **
-   *****************/
+  function executeConsensus(uint256 blockNumber) external onlyOwner {
+    bytes32[] storage blockReports = reportsByBlock[blockNumber];
+    uint256 maxVotes = 0;
+    bytes32 consensusReportHash;
 
-  event SetBunkerMode(bool bunkerMode);
-  event AddOracle(address oracle);
-  event RemoveOracle(address oracle);
-  event AddOracleBlacklist(address oracle);
-  event RemoveOracleBlacklist(address oracle);
+    // We iterate through each report once
+    for (uint256 i = 0; i < blockReports.length; i++) {
+      bytes32 currentReportHash = blockReports[i];
+      uint256 currentVotes = 0;
 
-  function getOracles() external view returns (address[] memory) {
-    return oracles;
-  }
+      // And count how many reports are equal to it
+      for (uint256 j = 0; j < blockReports.length; j++) {
+        if (currentReportHash == blockReports[j]) {
+          currentVotes++;
+        }
+      }
 
-  function isOracle(address _oracle) public view returns (bool) {
-    if (oraclesBlacklist[_oracle]) {
-      return false;
-    }
-    for (uint256 i = 0; i < oracles.length; i++) {
-      if (oracles[i] == _oracle) {
-        return true;
+      // We update the consensusReportHash and maxVotes
+      // if currentVotes is greater than maxVotes
+      if (currentVotes > maxVotes) {
+        consensusReportHash = currentReportHash;
+        maxVotes = currentVotes;
       }
     }
-    return false;
-  }
 
-  function _addOracle(address _oracle) internal onlyOwner {
-    require(!isOracle(_oracle), 'ORACLE_ALREADY_EXISTS');
-    require(_oracle != address(0), 'Oracle address cannot be zero');
-    oracles.push(_oracle);
-    _updateQuorum();
-    emit AddOracle(_oracle);
-  }
+    // If we got more votes than the oracleQuorum,
+    // we consider it a valid report and update the state
+    if (maxVotes >= oracleQuorum) {
+      Report memory consensusReport = reports[consensusReportHash];
 
-  function _removeOracle(address _oracle) internal onlyOwner {
-    require(isOracle(_oracle), 'ORACLE_DOES_NOT_EXIST');
+      // Todo: Integrate Stake Together
 
-    for (uint256 i = 0; i < oracles.length; i++) {
-      if (oracles[i] == _oracle) {
-        oracles[i] = oracles[oracles.length - 1];
-        oracles.pop();
-        break;
-      }
+      reportNextBlock = consensusReport.blockNumber + 1;
+      emit ConsensusApproved(blockNumber, consensusReportHash);
+    } else {
+      reportNextBlock = blockNumber + reportFrequency;
+      emit ConsensusFail(blockNumber, consensusReportHash);
     }
-    emit RemoveOracle(_oracle);
-
-    if (oracles.length < reportQuorum) {
-      _pause();
-    }
-    _updateQuorum();
   }
 
-  function _addOracleBlacklist(address _oracle) internal onlyOwner {
-    require(isOracle(_oracle), 'NODE_DOES_NOT_EXIST');
-    require(_oracle != address(0), 'Oracle address cannot be zero');
-    oraclesBlacklist[_oracle] = true;
-    emit AddOracleBlacklist(_oracle);
-  }
-
-  function _removeOracleBlacklist(address _oracle) internal onlyOwner {
-    require(oraclesBlacklist[_oracle], 'NODE_NOT_UN_BLACKLISTED');
-    oraclesBlacklist[_oracle] = false;
-    emit RemoveOracleBlacklist(_oracle);
-  }
-
-  function setStakeTogether(address _stakeTogether) external onlyOwner {
-    require(address(stakeTogether) == address(0), 'STAKE_TOGETHER_ALREADY_SET');
-    stakeTogether = StakeTogether(payable(_stakeTogether));
-    emit SetStakeTogether(_stakeTogether);
-  }
-
-  function setBunkerMode(bool _bunkerMode) external onlyOwner {
-    bunkerMode = _bunkerMode;
-    emit SetBunkerMode(_bunkerMode);
-  }
-
-  function setRewardsSanityLimit(uint256 _rewardsSanityLimit) external onlyOwner {
-    rewardsSanityLimit = _rewardsSanityLimit;
-    emit SetRewardsSanityLimit(_rewardsSanityLimit);
-  }
-
-  function _updateQuorum() internal {
-    uint256 newQuorum = (oracles.length + 1) / 2;
-    newQuorum = newQuorum < 3 ? 3 : newQuorum;
-    reportQuorum = newQuorum;
-    emit SetReportQuorum(newQuorum);
+  function isReportReady(uint256 blockNumber) public view returns (bool) {
+    return reportsByBlock[blockNumber].length >= oracleQuorum;
   }
 }
