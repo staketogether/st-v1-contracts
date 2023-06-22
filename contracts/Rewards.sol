4f518871308fcd08916f4ed3946ce3cbccc2aa79
Fabricio Miranda <fabriciomirandabr@gmail.com>
2023-06-22 16:59:26 -0300

    feat: add validation

--- contracts/Rewards.sol
+++ contracts/Rewards.sol
@@ -36,6 +36,11 @@ contract Rewards is Ownable, Pausable, ReentrancyGuard {
 
   event ProposeTimeLockAction(string action, uint256 value, address target, uint256 executionTime);
   event ExecuteTimeLockAction(string action);
+  event SetTimeLockDuration(uint256 newDuration);
+  event SetOraclePenalizeLimit(uint256 newLimit);
+  event SetBlockReportGrowthLimit(uint256 newLimit);
+
+  // Todo: add missing events
 
   struct TimeLockedProposal {
     uint256 value;
@@ -67,12 +72,17 @@ contract Rewards is Ownable, Pausable, ReentrancyGuard {
 
     if (keccak256(bytes(action)) == keccak256(bytes('setTimeLockDuration'))) {
       timeLockDuration = proposal.value;
+      emit SetTimeLockDuration(proposal.value);
     } else if (keccak256(bytes(action)) == keccak256(bytes('addOracle'))) {
       _addOracle(proposal.target);
     } else if (keccak256(bytes(action)) == keccak256(bytes('removeOracle'))) {
       _removeOracle(proposal.target);
-    } else if (keccak256(bytes(action)) == keccak256(bytes('setPenalize'))) {
-      penalizeLimit = proposal.value;
+    } else if (keccak256(bytes(action)) == keccak256(bytes('setOraclePenalizeLimit'))) {
+      oraclePenalizeLimit = proposal.value;
+      emit SetOraclePenalizeLimit(proposal.value);
+    } else if (keccak256(bytes(action)) == keccak256(bytes('setBlockReportLimit'))) {
+      reportGrowthLimit = proposal.value;
+      emit SetBlockReportGrowthLimit(proposal.value);
     }
 
     // Todo: Add missing operations
@@ -91,7 +101,10 @@ contract Rewards is Ownable, Pausable, ReentrancyGuard {
    *****************/
 
   modifier onlyOracle() {
-    require(activeOracles[msg.sender] && oraclesBlacklist[msg.sender] < penalizeLimit, 'ONLY_ORACLES');
+    require(
+      activeOracles[msg.sender] && oraclesBlacklist[msg.sender] < oraclePenalizeLimit,
+      'ONLY_ORACLES'
+    );
     _;
   }
 
@@ -112,7 +125,7 @@ contract Rewards is Ownable, Pausable, ReentrancyGuard {
   mapping(address => bool) private activeOracles;
   mapping(address => uint256) public oraclesBlacklist;
   uint256 public oracleQuorum = 1; // Todo: Mainnet = 3
-  uint256 public penalizeLimit = 3;
+  uint256 public oraclePenalizeLimit = 3;
   bool public bunkerMode = false;
 
   function getOracles() external view returns (address[] memory) {
@@ -130,7 +143,7 @@ contract Rewards is Ownable, Pausable, ReentrancyGuard {
   }
 
   function isOracle(address _oracle) public view returns (bool) {
-    return activeOracles[_oracle] && oraclesBlacklist[_oracle] < penalizeLimit;
+    return activeOracles[_oracle] && oraclesBlacklist[_oracle] < oraclePenalizeLimit;
   }
 
   function setBunkerMode(bool _bunkerMode) external onlyOwner {
@@ -167,7 +180,7 @@ contract Rewards is Ownable, Pausable, ReentrancyGuard {
   function _penalizeOracle(address oracle, bytes32 faultyReportHash) internal {
     oraclesBlacklist[oracle]++;
 
-    bool remove = oraclesBlacklist[oracle] >= penalizeLimit;
+    bool remove = oraclesBlacklist[oracle] >= oraclePenalizeLimit;
     if (remove) {
       _removeOracle(oracle);
     }
@@ -202,10 +215,10 @@ contract Rewards is Ownable, Pausable, ReentrancyGuard {
     uint256 operatorShares;
     uint256 poolShares;
     uint256 poolsToSubmit;
-    uint256 poolSubmitted;
-    uint256 poolSharesSubmitted;
+    uint256 poolsSubmitted;
+    uint256 poolsSharesSubmitted;
     bool consensusExecuted;
-    bytes32[] exitedValidators;
+    bytes[] exitedValidators;
   }
 
   mapping(bytes32 => BlockReport) public blockReports;
@@ -213,6 +226,7 @@ contract Rewards is Ownable, Pausable, ReentrancyGuard {
   mapping(address => mapping(uint256 => bool)) public oracleBlockReport;
   mapping(bytes32 => uint256) public blockReportsVotes;
 
+  uint256 public reportGrowthLimit = 1;
   uint256 public reportLastBlock = 0;
   uint256 public reportNextBlock = 1;
   uint256 public reportFrequency = 1;
@@ -225,7 +239,7 @@ contract Rewards is Ownable, Pausable, ReentrancyGuard {
     uint256 stakeTogetherShares,
     uint256 operatorShares,
     uint256 poolShares,
-    bytes32[] calldata exitedValidators,
+    bytes[] calldata exitedValidators,
     uint256 poolsToSubmit
   ) external onlyOracle whenNotPaused {
     require(blockNumber != reportNextBlock, 'INVALID_REPORT_BLOCK_NUMBER');
@@ -247,8 +261,6 @@ contract Rewards is Ownable, Pausable, ReentrancyGuard {
       exitedValidators
     );
 
-    _blockReportValidator(blockReport);
-
     bytes32 reportHash = keccak256(
       abi.encode(
         blockNumber,
@@ -262,6 +274,8 @@ contract Rewards is Ownable, Pausable, ReentrancyGuard {
         poolsToSubmit
       )
     );
+
+    _blockSubmitReportValidator(blockReport, reportHash);
 
     blockReports[reportHash] = blockReport;
     blockReportsVotes[reportHash]++;
@@ -278,8 +292,8 @@ contract Rewards is Ownable, Pausable, ReentrancyGuard {
     uint256 votes = blockReportsVotes[consensusReportHash];
     BlockReport storage consensusReport = blockReports[consensusReportHash];
 
-    bool isConsensusApproved = consensusReport.poolShares == consensusReport.poolSharesSubmitted &&
-      consensusReport.poolsToSubmit == consensusReport.poolSubmitted;
+    bool isConsensusApproved = consensusReport.poolShares == consensusReport.poolsSharesSubmitted &&
+      consensusReport.poolsToSubmit == consensusReport.poolsSubmitted;
 
     if (votes >= oracleQuorum && isConsensusApproved) {
       consensusReport.consensusExecuted = true;
@@ -297,31 +311,73 @@ contract Rewards is Ownable, Pausable, ReentrancyGuard {
     return blockReportsVotes[reportHash] >= oracleQuorum;
   }
 
-  function _blockReportValidator(BlockReport memory _blockReport) private pure {
+  function _blockSubmitReportValidator(
+    BlockReport memory _blockReport,
+    bytes32 _reportHash
+  ) private view {
     // Todo: Implement Validations
-    require(_blockReport.blockNumber > 0, 'INVALID_BLOCK_NUMBER');
-    require(_blockReport.beaconBalance > 0, 'INVALID_BEACON_BALANCE');
+    require(_blockReport.blockNumber == reportNextBlock, 'BLOCK_REPORT_IS_NOT_NEXT_EXPECTED');
+    require(_blockReport.beaconBalance >= 0, 'INVALID_BEACON_BALANCE');
     require(_blockReport.totalRewardsAmount >= 0, 'INVALID_TOTAL_REWARDS_AMOUNT');
-    require(_blockReport.totalRewardsShares > 0, 'INVALID_TOTAL_REWARDS_SHARES');
-    require(_blockReport.stakeTogetherShares > 0, 'INVALID_STAKE_TOGETHER_SHARES');
+    require(_blockReport.totalRewardsShares >= 0, 'INVALID_TOTAL_REWARDS_SHARES');
+    require(_blockReport.stakeTogetherShares >= 0, 'INVALID_STAKE_TOGETHER_SHARES');
     require(_blockReport.operatorShares >= 0, 'INVALID_OPERATOR_SHARES');
     require(_blockReport.poolShares >= 0, 'INVALID_POOL_SHARES');
-    require(_blockReport.poolsToSubmit > 0, 'INVALID_POOLS_TO_SUBMIT');
-    require(_blockReport.exitedValidators.length > 0, 'INVALID_EXITED_VALIDATORS');
-
-    // Outras validações personalizadas podem ser adicionadas aqui
-    // Por exemplo, validar a consistência entre diferentes campos ou realizar cálculos de integridade.
-
-    // Exemplo de validação da consistência entre poolsToSubmit, poolSubmitted e poolSharesSubmitted
-    require(_blockReport.poolsToSubmit >= _blockReport.poolSubmitted, 'INVALID_POOL_SUBMITTED');
+    require(_blockReport.poolsToSubmit >= 0, 'INVALID_POOLS_TO_SUBMIT');
+    require(_blockReport.poolsSubmitted == 0, 'INVALID_POOL_SUBMITTED');
+    require(_blockReport.poolsSharesSubmitted == 0, 'INVALID_POOL_SUBMITTED');
+    require(_blockReport.consensusExecuted == false, 'INVALID_CONSENSUS_EXECUTED');
     require(
-      _blockReport.poolsToSubmit >= _blockReport.poolSharesSubmitted,
-      'INVALID_POOL_SHARES_SUBMITTED'
+      _blockReport.exitedValidators.length >= 0 && _blockReport.exitedValidators.length <= 64,
+      'INVALID_EXITED_VALIDATORS'
     );
+
+    uint256 totalPooledEther = stakeTogether.totalPooledEther();
+    uint256 growthLimit = Math.mulDiv(totalPooledEther, reportGrowthLimit, 100);
+    require(_blockReport.totalRewardsAmount <= growthLimit, 'GROWTH_LIMIT_EXCEEDED');
+
+    uint256 stakeTogetherFee = Math.mulDiv(totalPooledEther, stakeTogether.stakeTogetherFee(), 100);
+    uint256 operatorFee = Math.mulDiv(totalPooledEther, stakeTogether.operatorFee(), 100);
+    uint256 poolFee = Math.mulDiv(totalPooledEther, stakeTogether.poolFee(), 100);
+    require(
+      stakeTogether.pooledEthByShares(_blockReport.stakeTogetherShares) <= stakeTogetherFee,
+      'STAKE_TOGETHER_FEE_EXCEEDED'
+    );
+    require(
+      stakeTogether.pooledEthByShares(_blockReport.operatorShares) <= operatorFee,
+      'OPERATOR_FEE_EXCEEDED'
+    );
+    require(stakeTogether.pooledEthByShares(_blockReport.poolShares) <= poolFee, 'POOL_FEE_EXCEEDED');
+
+    uint256 totalShares = _blockReport.stakeTogetherShares +
+      _blockReport.operatorShares +
+      _blockReport.poolShares;
+    require(totalShares == _blockReport.totalRewardsShares, 'INVALID_TOTAL_SHARES');
+
+    for (uint i = 0; i < _blockReport.exitedValidators.length; i++) {
+      require(stakeTogether.isValidator(_blockReport.exitedValidators[i]), 'INVALID_EXITED_VALIDATOR');
+    }
+
+    uint256 expectedMaxBeaconBalance = stakeTogether.validatorSize() * stakeTogether.totalValidators();
+    require(
+      _blockReport.beaconBalance <= expectedMaxBeaconBalance,
+      'BEACON_BALANCE_EXCEEDS_EXPECTED_MAX'
+    );
+
+    require(!blockReports[_reportHash].consensusExecuted, 'BLOCK_ALREADY_EXECUTED');
+  }
+
+  function _blockExecuteReportValidator() private {
+    // Validate that the report is not executed
+    // Validate that pools submitted are the same as pools to submit
+    // Validate that pools shares submitted are the same as pools shares to submit
   }
 
   function _blockReportActions(BlockReport memory _blockReport) private {
     // Todo: Implement Actions
+    // stakeTogether.setBeaconBalance(_blockReport.beaconBalance);
+    // stakeTogether mintSharesRewards (public function only for oracle)
+    // transfer ethereum to stake together (pooledEtherByShares)
   }
 
   /*****************
