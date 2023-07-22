7e2df9edd1c4ae71a7be61f698220fd1af1c4f75
Fabricio Miranda <fabriciomirandabr@gmail.com>
2023-07-22 19:31:13 -0300

    feat: implement operational functions

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -23,6 +23,12 @@ contract Lock is AccessControl, Pausable, ReentrancyGuard {
   uint256 public maxDays;
   uint256 public incentiveFactor;
 
+  uint256 public minLockAmount = 1 ether;
+  uint256 public maxLockedAmount = 100000 ether;
+  uint256 public totalLockedAmount;
+
+  bool enableLock = true;
+
   event MintRewardsAccounts(address indexed sender, uint amount);
   event MintRewardsAccountsFallback(address indexed sender, uint amount);
   event LockToken(address indexed user, uint256 amount, uint256 lockDays);
@@ -30,6 +36,9 @@ contract Lock is AccessControl, Pausable, ReentrancyGuard {
   event SetIncentiveFactor(uint256 amount);
   event SetMinDays(uint256 amount);
   event SetMaxDays(uint256 amount);
+  event SetEnableLock(bool enableLock);
+  event SetMinLockAmount(uint256 amount);
+  event SetMaxLockedAmount(uint256 amount);
 
   constructor(address _stakeTogether, uint256 _minDays, uint256 _maxDays, uint256 _incentiveFactor) {
     stakeTogether = IERC20(_stakeTogether);
@@ -64,11 +73,14 @@ contract Lock is AccessControl, Pausable, ReentrancyGuard {
   }
 
   function lockToken(uint256 _amount, uint256 _days) external whenNotPaused nonReentrant {
+    require(enableLock, 'LOCK_DISABLED');
     require(_days >= minDays && _days <= maxDays, 'INVALID_DAYS');
     uint256 newLockTime = block.timestamp + (_days * 1 days);
     require(newLockTime >= lockTime[msg.sender], 'CANNOT_REDUCE_LOCK_TIME');
     uint256 allowance = stakeTogether.allowance(msg.sender, address(this));
     require(allowance >= _amount, 'ALLOWANCE_NOT_ENOUGH');
+    require(_amount >= minLockAmount, 'AMOUNT_TOO_LOW');
+    require(_amount + totalLockedAmount <= maxLockedAmount, 'MAX_LOCKED_AMOUNT_EXCEEDED');
 
     stakeTogether.transferFrom(msg.sender, address(this), _amount);
     balances[msg.sender] += _amount;
@@ -77,25 +89,30 @@ contract Lock is AccessControl, Pausable, ReentrancyGuard {
     uint256 shares = _amount * incentiveFactorOf(msg.sender);
     lockShares[msg.sender] += shares;
     totalLockShares += shares;
+    totalLockedAmount += _amount;
 
     emit LockToken(msg.sender, _amount, _days);
   }
 
-  function unlockToken() external whenNotPaused nonReentrant {
+  function unlockToken(uint256 _amount) external whenNotPaused nonReentrant {
     require(block.timestamp > lockTime[msg.sender], 'LOCK_TIME_NOT_PASSED');
+    require(_amount <= balances[msg.sender], 'AMOUNT_TOO_HIGH');
 
-    uint256 shares = balances[msg.sender] * incentiveFactorOf(msg.sender);
+    uint256 shares = _amount * incentiveFactorOf(msg.sender);
 
     require(lockShares[msg.sender] >= shares, 'LOCK_SHARES_NOT_ENOUGH');
 
     lockShares[msg.sender] -= shares;
     totalLockShares -= shares;
+    totalLockedAmount -= _amount;
 
-    uint256 amountToTransfer = balances[msg.sender];
-    balances[msg.sender] = 0;
-    stakeTogether.transfer(msg.sender, amountToTransfer);
+    uint256 incentiveDecrease = Math.mulDiv(incentiveFactor, _amount, totalLockedAmount);
+    incentiveFactor -= incentiveDecrease;
 
-    emit UnlockToken(msg.sender, amountToTransfer);
+    balances[msg.sender] -= _amount;
+    stakeTogether.transfer(msg.sender, _amount);
+
+    emit UnlockToken(msg.sender, _amount);
   }
 
   function incentiveFactorOf(address _account) public view returns (uint256) {
@@ -132,5 +149,20 @@ contract Lock is AccessControl, Pausable, ReentrancyGuard {
     require(_maxDays > 0, 'INVALID_MAX_DAYS');
     maxDays = _maxDays;
     emit SetMaxDays(_maxDays);
+  }
+
+  function setEnableLock(bool _enableLock) external onlyRole(ADMIN_ROLE) {
+    enableLock = _enableLock;
+    emit SetEnableLock(_enableLock);
+  }
+
+  function setMinLockAmount(uint256 _minLockAmount) external onlyRole(ADMIN_ROLE) {
+    minLockAmount = _minLockAmount;
+    emit SetMinLockAmount(_minLockAmount);
+  }
+
+  function setMaxLockedAmount(uint256 _maxLockedAmount) external onlyRole(ADMIN_ROLE) {
+    maxLockedAmount = _maxLockedAmount;
+    emit SetMaxLockedAmount(_maxLockedAmount);
   }
 }
