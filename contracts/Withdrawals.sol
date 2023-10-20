3a92552f96767ac29e914d284cff9d027cc99481
Fabricio Miranda <fabriciomirandabr@gmail.com>
2023-10-20 17:52:49 -0300

    Feat: Improve Security (#19) (#20)
    * feat: updated Open Zeppelin 5.0
    
    * feat: add custom error for airdrop
    
    * feat: sort
    
    * feat: add custom errors
    
    * feat: add custom errors
    
    * feat: add custom errors
    
    * feat: sort custom errors
    
    * fix: remove deprecated functions OPZ5
    
    * fix: add missing custom error
    
    * feat: add anti-fraud
    
    * fix: pause transfer
    
    * feat: update main domain
    
    * feat: update domain
    
    * feat: improve name
    
    * feat: improve patterns
    
    * feat: add wstpETH
    
    * feat: add wstpETH
    
    * feat: update airdrop contracts test
    
    * feat: update router tests
    
    * feat: update withdrawals tests
    
    * feat: update stake together tests
    
    * feat: add gas reporter
    
    * fix: env
    
    * feat: add deploy ST Wrapper
    
    * feat: update packages
    
    * fix: set version
    
    * feat: update packages
    
    * feat: add anti fraud test
    
    * feat: add not listed test
    
    * feat: add tests to withdrawals
    
    * feat: add test on withdrawals
    
    * feat: add more tests
    
    * feat: add more tests on wstpETH
    
    * feat: add test on stpETH balance
    
    * feat: add wrap tests
    
    * feat: add tests on submit report

--- contracts/Withdrawals.sol
+++ contracts/Withdrawals.sol
@@ -1,25 +1,24 @@
-// SPDX-FileCopyrightText: 2023 Stake Together Labs <legal@staketogether.app>
+// SPDX-FileCopyrightText: 2023 Stake Together Labs <legal@staketogether.org>
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.20;
 
 import '@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol';
 import '@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol';
 import '@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol';
-import '@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol';
-import '@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol';
+import '@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol';
+import '@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol';
 import '@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol';
 import '@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol';
 import '@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol';
 
-import './Router.sol';
-import './StakeTogether.sol';
-import './interfaces/IWithdrawals.sol';
+import './interfaces/IRouter.sol';
 import './interfaces/IStakeTogether.sol';
+import './interfaces/IWithdrawals.sol';
 
 /// @title Withdrawals Contract for StakeTogether
 /// @notice The Withdrawals contract handles all withdrawal-related activities within the StakeTogether protocol.
 /// It allows users to withdraw their staked tokens and interact with the associated stake contracts.
-/// @custom:security-contact security@staketogether.app
+/// @custom:security-contact security@staketogether.org
 contract Withdrawals is
   Initializable,
   ERC20Upgradeable,
@@ -35,8 +34,8 @@ contract Withdrawals is
   bytes32 public constant ADMIN_ROLE = keccak256('ADMIN_ROLE'); /// Role for administration.
 
   uint256 public version; /// Contract version.
-  StakeTogether public stakeTogether; /// Instance of the StakeTogether contract.
-  Router public router; /// Instance of the Router contract.
+  IStakeTogether public stakeTogether; /// Instance of the StakeTogether contract.
+  IRouter public router; /// Instance of the Router contract.
 
   /// @custom:oz-upgrades-unsafe-allow constructor
   constructor() {
@@ -82,7 +81,7 @@ contract Withdrawals is
   /// @notice Allows the router to send ETH to the contract.
   /// @dev This function can only be called by the router.
   function receiveWithdrawEther() external payable {
-    require(msg.sender == address(router), 'ONLY_ROUTER');
+    if (msg.sender != address(router)) revert OnlyRouter();
     emit ReceiveWithdrawEther(msg.value);
   }
 
@@ -90,7 +89,7 @@ contract Withdrawals is
   /// @dev Only callable by the admin role. Requires that extra amount exists in the contract balance.
   function transferExtraAmount() external whenNotPaused nonReentrant onlyRole(ADMIN_ROLE) {
     uint256 extraAmount = address(this).balance - totalSupply();
-    require(extraAmount > 0, 'NO_EXTRA_AMOUNT');
+    if (extraAmount <= 0) revert NoExtraAmountAvailable();
     address stakeTogetherFee = stakeTogether.getFeeAddress(IStakeTogether.FeeRole.StakeTogether);
     payable(stakeTogetherFee).transfer(extraAmount);
   }
@@ -99,8 +98,9 @@ contract Withdrawals is
   /// @param _stakeTogether The address of the new StakeTogether contract.
   /// @dev Only callable by the admin role.
   function setStakeTogether(address _stakeTogether) external onlyRole(ADMIN_ROLE) {
-    require(_stakeTogether != address(0), 'STAKE_TOGETHER_ALREADY_SET');
-    stakeTogether = StakeTogether(payable(_stakeTogether));
+    if (address(stakeTogether) != address(0)) revert StakeTogetherAlreadySet();
+    if (address(_stakeTogether) == address(0)) revert ZeroAddress();
+    stakeTogether = IStakeTogether(payable(_stakeTogether));
     emit SetStakeTogether(_stakeTogether);
   }
 
@@ -108,22 +108,53 @@ contract Withdrawals is
   /// @param _router The address of the router.
   /// @dev Only callable by the admin role.
   function setRouter(address _router) external onlyRole(ADMIN_ROLE) {
-    require(_router != address(0), 'ROUTER_CONTRACT_ALREADY_SET');
-    router = Router(payable(_router));
+    if (address(router) != address(0)) revert RouterAlreadySet();
+    if (address(_router) == address(0)) revert ZeroAddress();
+    router = IRouter(payable(_router));
     emit SetRouter(_router);
   }
 
-  /// @notice Hook that is called before any token transfer.
-  /// @param from Address transferring the tokens.
-  /// @param to Address receiving the tokens.
-  /// @param amount The amount of tokens to be transferred.
-  /// @dev This override ensures that transfers are paused when the contract is paused.
-  function _beforeTokenTransfer(
-    address from,
-    address to,
-    uint256 amount
-  ) internal override whenNotPaused {
-    super._beforeTokenTransfer(from, to, amount);
+  /****************
+   ** ANTI-FRAUD **
+   ****************/
+
+  /// @notice Transfers an amount of wei to the specified address.
+  /// @param _to The address to transfer to.
+  /// @param _amount The amount to be transferred.
+  /// @return True if the transfer was successful.
+  function transfer(
+    address _to,
+    uint256 _amount
+  ) public override(ERC20Upgradeable, IWithdrawals) returns (bool) {
+    if (stakeTogether.isListedInAntiFraud(msg.sender)) revert ListedInAntiFraud();
+    if (stakeTogether.isListedInAntiFraud(_to)) revert ListedInAntiFraud();
+    _transfer(msg.sender, _to, _amount);
+    return true;
+  }
+
+  /// @notice Transfers tokens from one address to another using an allowance mechanism.
+  /// @param _from Address to transfer from.
+  /// @param _to Address to transfer to.
+  /// @param _amount Amount of tokens to transfer.
+  /// @return A boolean value indicating whether the operation succeeded.
+  function transferFrom(
+    address _from,
+    address _to,
+    uint256 _amount
+  ) public override(ERC20Upgradeable, IWithdrawals) returns (bool) {
+    if (stakeTogether.isListedInAntiFraud(_from)) revert ListedInAntiFraud();
+    if (stakeTogether.isListedInAntiFraud(_to)) revert ListedInAntiFraud();
+    _spendAllowance(_from, msg.sender, _amount);
+    _transfer(_from, _to, _amount);
+    return true;
+  }
+
+  /// @notice Transfers an amount of wei from one address to another.
+  /// @param _from The address to transfer from.
+  /// @param _to The address to transfer to.
+  /// @param _amount The amount to be transferred.
+  function _update(address _from, address _to, uint256 _amount) internal override whenNotPaused {
+    super._update(_from, _to, _amount);
   }
 
   /**************
@@ -135,7 +166,7 @@ contract Withdrawals is
   /// @param _amount Amount of tokens to mint.
   /// @dev Only callable by the StakeTogether contract.
   function mint(address _to, uint256 _amount) public whenNotPaused {
-    require(msg.sender == address(stakeTogether), 'ONLY_STAKE_TOGETHER_CONTRACT');
+    if (msg.sender != address(stakeTogether)) revert OnlyStakeTogether();
     _mint(_to, _amount);
   }
 
@@ -143,9 +174,10 @@ contract Withdrawals is
   /// @param _amount Amount of ETH to withdraw.
   /// @dev The caller must have a balance greater or equal to the amount, and the contract must have sufficient ETH balance.
   function withdraw(uint256 _amount) public whenNotPaused nonReentrant {
-    require(address(this).balance >= _amount, 'INSUFFICIENT_ETH_BALANCE');
-    require(balanceOf(msg.sender) >= _amount, 'INSUFFICIENT_STW_BALANCE');
-    require(_amount > 0, 'ZERO_AMOUNT');
+    if (stakeTogether.isListedInAntiFraud(msg.sender)) revert ListedInAntiFraud();
+    if (address(this).balance < _amount) revert InsufficientEthBalance();
+    if (balanceOf(msg.sender) < _amount) revert InsufficientStwBalance();
+    if (_amount <= 0) revert ZeroAmount();
     emit Withdraw(msg.sender, _amount);
     _burn(msg.sender, _amount);
     payable(msg.sender).transfer(_amount);
@@ -155,6 +187,7 @@ contract Withdrawals is
   /// @param _amount Amount of ETH to check.
   /// @return A boolean indicating if the contract has sufficient balance to withdraw the specified amount.
   function isWithdrawReady(uint256 _amount) public view returns (bool) {
+    if (stakeTogether.isListedInAntiFraud(msg.sender)) return false;
     return address(this).balance >= _amount;
   }
 }
