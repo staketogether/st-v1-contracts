42b2c3e549401247c74340c0596bafcce2091cfb
Fabricio Miranda <fabriciomirandabr@gmail.com>
2023-11-24 12:16:33 -0300

    fix: fix version

--- contracts/mocks/MockFlashLoan.sol
+++ contracts/mocks/MockFlashLoan.sol
@@ -1,5 +1,6 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+// SPDX-FileCopyrightText: 2023 Stake Together Labs <legal@staketogether.org>
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.22;
 
 import '@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol';
 import '@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol';
