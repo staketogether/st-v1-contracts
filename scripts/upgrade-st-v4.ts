c997290093b53dbf0a60e64846ba2f32b03797c0
Fabricio Miranda <fabriciomirandabr@gmail.com>
2024-01-17 14:16:50 -0300

    feat: transfer on claim

---
+++ scripts/upgrade-st-v4.ts
@@ -0,0 +1,32 @@
+import { getImplementationAddress } from '@openzeppelin/upgrades-core'
+import { ethers, network, upgrades } from 'hardhat'
+import { StakeTogetherV4__factory } from '../typechain'
+
+const proxyAddress = '0x218dE5E6324c5351C3a2bf0c40d76f585B8dE04d'
+
+async function upgrade() {
+  const [owner] = await ethers.getSigners()
+
+  const StakeTogether = new StakeTogetherV4__factory(owner)
+
+  console.log(`Upgrading StakeTogetherV4...`)
+
+  const upgradedContract = await upgrades.upgradeProxy(proxyAddress, StakeTogether, {
+    redeployImplementation: 'always',
+  })
+  console.log(`Upgraded To StakeTogetherV4`)
+
+  console.log('Initializing V4...')
+  await upgradedContract.initializeV4()
+  console.log('Initialized V4')
+
+  console.log('Waiting Abi Propagate...')
+  await new Promise((resolve) => setTimeout(resolve, 30000))
+
+  console.log('Get Implementation for Verify... Double Check')
+  const implementationAddress = await getImplementationAddress(network.provider, proxyAddress)
+
+  console.log(`npx hardhat verify --network ${network.name} ${implementationAddress}`)
+}
+
+upgrade()
