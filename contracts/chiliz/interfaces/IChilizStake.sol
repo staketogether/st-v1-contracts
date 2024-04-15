fc842078c7560b87fa25b488104eba6acc691f3b
Fabricio Miranda <fabriciomirandabr@gmail.com>
2024-04-15 17:03:36 +0300

    feat: update chiliz contracts

---
+++ contracts/chiliz/interfaces/IChilizStake.sol
@@ -0,0 +1,10 @@
+// SPDX-FileCopyrightText: 2024 Together Technology LTD <legal@staketogether.org>
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.25;
+
+/// @title Interface for Chiliz Staking
+/// @notice A contract that represent the validator withdrawal functionality
+/// @custom:security-contact security@staketogether.org
+interface IChilizStake {
+
+}
